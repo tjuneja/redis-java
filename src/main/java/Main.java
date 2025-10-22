ca9e57da9a8947a03d5b8513212ae28997c5e8b6
tushar-zeda <tushar.juneja@zeda.io>
2025-10-22 13:27:44 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,63 +12,63 @@ import java.util.Set;
 
 public class Main {
 
-  ServerSocketChannel serverChannel;
-  Selector selector;
-  ByteBuffer readBuffer;
+    ServerSocketChannel serverChannel;
+    Selector selector;
+    ByteBuffer readBuffer = ByteBuffer.allocate(1024);
 
-  public  void start(int port) throws IOException {
-    setupServer(port);
-  }
+    public void start(int port) throws IOException {
+        setupServer(port);
+    }
 
-  private void setupServer(int port) throws IOException {
-      serverChannel = ServerSocketChannel.open();
-      selector = Selector.open();
-      serverChannel.configureBlocking(false);
-      serverChannel.bind(new InetSocketAddress(port));
-      serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-      System.out.println("Server started on port : "+ port);
-      eventLoop();
-  }
+    private void setupServer(int port) throws IOException {
+        serverChannel = ServerSocketChannel.open();
+        selector = Selector.open();
+        serverChannel.configureBlocking(false);
+        serverChannel.bind(new InetSocketAddress(port));
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        System.out.println("Server started on port : "+ port);
+        eventLoop();
+    }
 
     private void eventLoop() throws IOException {
-      while (true){
-          selector.select();
+        while (true){
+            selector.select();
 
-          Set<SelectionKey> selectedKeys = selector.selectedKeys();
-          Iterator<SelectionKey> iterator = selectedKeys.iterator();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectedKeys.iterator();
 
-          while (iterator.hasNext()){
-              SelectionKey key = iterator.next();
-              iterator.remove();;
+            while (iterator.hasNext()){
+                SelectionKey key = iterator.next();
+                iterator.remove();;
 
-              if(!key.isValid()) continue;
+                if(!key.isValid()) continue;
 
-              try {
-                  if(key.isAcceptable()){
-                      handleAccept(key);
-                  } else if (key.isReadable()) {
-                      handleRead(key);
-                  } else if (key.isWritable()) {
-                      handleWrite(key);
-                  }
-              }catch (IOException e){
-                  System.out.println();
-                  closeChannel(key);
-              }
-          }
-      }
+                try {
+                    if(key.isAcceptable()){
+                        handleAccept(key);
+                    } else if (key.isReadable()) {
+                        handleRead(key);
+                    } else if (key.isWritable()) {
+                        handleWrite(key);
+                    }
+                }catch (IOException e){
+                    System.out.println();
+                    closeChannel(key);
+                }
+            }
+        }
 
 
     }
 
     private void closeChannel(SelectionKey key) {
-      try {
-          key.channel().close();
-          key.cancel();
+        try {
+            key.channel().close();
+            key.cancel();
 
-      }catch (IOException e){
-          e.printStackTrace();
-      }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     private void handleWrite(SelectionKey key) {
@@ -95,10 +95,10 @@ public class Main {
             System.out.println("Data received : "+ str);
 
             //Echo back
-            String response = "+PONG\\r\\n";
+            String response = "+PONG\r\n";
             ByteBuffer writeBuffer = ByteBuffer.wrap(response.getBytes());
             clientChannel.write(writeBuffer);
-         }
+        }
 
     }
 
@@ -114,30 +114,9 @@ public class Main {
 
     }
 
-    public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) throws IOException {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        new Main().start(6379);
 
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
-        }
-  }
+    }
 }
